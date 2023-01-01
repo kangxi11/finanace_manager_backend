08cb156d5bd1fee36db5c82496f6bb80203ea233
Ryan Kang <rkang2009@gmail.com>
2022-12-31 19:01:31 -0500

    can add CIBC data

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var cors = require('cors');
 const AWS = require('aws-sdk');
 const config = require('./config.js');
 require('dotenv').config();
@@ -7,6 +8,7 @@ const app = express()
 const port = 3000
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -41,28 +43,35 @@ app.get("/transactions", (req, res, next) => {
 app.post('/transactions', (req, res) => {
     AWS.config.update(config.aws_remote_config);
     const docClient = new AWS.DynamoDB.DocumentClient();
-    const transaction = { ...req.body };
-
-    var params = {
-        TableName: config.aws_table_name,
-        Item: transaction
-    };
-
-    // Call DynamoDB to add the item to the table
-    docClient.put(params, function (err, data) {
-        if (err) {
-            res.send({
-                success: false,
-                message: err
-            });
-        } else {
-            res.send({
-                success: true,
-                message: `Added transaction ${transaction.transaction}`
-            });
-        }
-    });    
-
+    const transactions = { ...req.body };
+    let successes = 0;
+    
+    transactions.transactions.forEach((t) => {
+        var params = {
+            TableName: config.aws_table_name,
+            Item: t
+        };
+    
+        // Call DynamoDB to add the item to the table
+        docClient.put(params, function (err, data) {
+            if (err) {
+                res.header("Access-Control-Allow-Origin", "*");
+                res.send({
+                    success: false,
+                    message: err
+                });
+            } else {
+                successes = successes + 1;
+                if (successes === transactions.transactions.length) {
+                    res.header("Access-Control-Allow-Origin", "*");
+                    res.send({
+                        success: true,
+                        message: "Added transactions"
+                    });
+                }
+            }
+        });    
+    });
 });
 
 app.delete('/transactions', (req, res) => {
